8b33f8bda6852f0461ba88e2a4ef9c0ca6d29287
Lauris Bessone <laurisbessonepro@gmail.com>
2022-06-13 15:49:47 +0200

    exercice fini

--- air00.rb
+++ air00.rb
@@ -1,1 +1,53 @@
-print "Hello \n", "Hello"
+
+#recupere le parametre pour la string serparer a chaque espaces
+# def cut_fonction(arr)
+#     for i in arr
+#         d = i.split(' ')
+#         puts d
+#     end
+# end
+
+def cut(cut_string, separator)
+   
+    
+    cut_string[0].each_line(separator) do |i|
+        if i == ' '
+        else puts i
+        end
+    end
+end
+
+#gestion d'erreur
+(puts "error"; exit) if ARGV[0] == nil
+
+
+#parsing
+arr = ARGV
+separator = ' '
+
+#resolution
+result = cut(arr, separator)
+#affichage
+result
+
+#ruby air00.rb "je suis la "
+
+=begin
+Créez un programme qui découpe une chaîne de caractères en tableau (séparateurs : espaces, tabulations,
+    retours à la ligne).
+    
+    Votre programme devra utiliser une fonction prototypée comme ceci :
+    ma_fonction(string_à_couper, string_séparateur) { // syntaxe selon votre langage
+        # votre algorithme
+        return (tableau)
+    }
+    
+    
+    Exemples d’utilisation :
+    $> python exo.py “Bonjour les gars”
+    Bonjour
+    les
+    gars
+    
+    Afficher error et quitter le programme en cas de problèmes d’arguments.
+=end
--- air01.rb
+++ air01.rb
@@ -1,1 +1,18 @@
-puts "hey"c
+=begin const args = process.argv.slice(2);
+
+for (let i = 0; i < args.length; i++) {
+    console.log(args[i]);
+}
+=end
+arr = ["Hello", "HallO", "Regted"]
+
+#map re map le tableau
+#select qui permet de selectionner des element de facon booléen
+#reduc
+ arr.map {|string| string.downcase}
+ [1, 2, 3, 4, 5, 6, 7, 8].select { |n| n % 2 == 0}
+
+arr.reduce({}) do |lengths, state|
+    lengths[state] = state.length
+    p lengths
+end
