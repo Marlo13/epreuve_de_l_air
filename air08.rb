556925f620b391afaf9cad5bc91cb054ed83aaed
Lauris Bessone <laurisbessonepro@gmail.com>
2022-06-17 15:18:30 +0200

    exercice 9 fini

--- air08.rb
+++ air08.rb
@@ -23,24 +23,6 @@ def sorted_fusion(array1, array2) #replacer dans un tableau les nombre trier des
     array4 = array3.join(' ')
     puts     array4.split(' ').sort.join(' ')
 
-    # while (a <= array1.size) && (b <= array2.size)
-    #  #p array1.size, array2.size, '!!'
-    #  p a, b, '??'
-    #     if array1[a].to_i > array2[b].to_i
-    #         array3 << array2[b]
-    #         b+=1
-    #     elsif array1[a].to_i < array2[b].to_i
-    #         array3 << array1[a]
-    #         a+=1
-    #     else array1[a].to_i == array2[b].to_i
-    #         array3 << array1[a]
-    #         array3 << array1[b]
-    #         a+=1
-    #         b+=1
-    #     end
-    #    #p array3
-    # end
-    # p array3
 end
 #gestion d'erreur
 (puts "error"; exit) if ARGV[0] == nil
@@ -60,7 +42,7 @@ result
 
 Utilisez une fonction de ce genre (selon votre langage) :
 sorted_fusion(array1, array2) {
-	# your algo
+    # your algo
 	return (new_array)
 }
 
@@ -72,3 +54,21 @@ $> ruby exo.rb 10 20 30 fusion 15 25 35
 
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 =end
+# while (a <= array1.size) && (b <= array2.size)
+#  #p array1.size, array2.size, '!!'
+#  p a, b, '??'
+#     if array1[a].to_i > array2[b].to_i
+#         array3 << array2[b]
+#         b+=1
+#     elsif array1[a].to_i < array2[b].to_i
+#         array3 << array1[a]
+#         a+=1
+#     else array1[a].to_i == array2[b].to_i
+#         array3 << array1[a]
+#         array3 << array1[b]
+#         a+=1
+#         b+=1
+#     end
+#    #p array3
+# end
+# p array3
